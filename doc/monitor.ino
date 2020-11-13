19519bc116771e66bb6cfd611ac25c52b1a2ff28
jleightcap <jleightcap@gmail.com>
2020-11-13 02:42:03 -0500

    cite Eater in monitor, set docs aside

--- doc/monitor.ino
+++ doc/monitor.ino
@@ -1,3 +1,7 @@
+// Full credit to Ben Eater,
+// see: https://eater.net/6502
+// slightly modified for use with Z80 in place of 6502
+
 const char ADDR[] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52};
 const char DATA[] = {39, 41, 43, 45, 47, 49, 51, 53};
 #define CLOCK 2
@@ -7,11 +11,11 @@ const char DATA[] = {39, 41, 43, 45, 47, 49, 51, 53};
 #define MREQ 6
 
 void setup() {
-  for (int n = 0; n < 16; n += 1) {
-    pinMode(ADDR[n], INPUT);
+  for(unsigned int nn = 0; nn < 16; nn++) {
+    pinMode(ADDR[nn], INPUT);
   }
-  for (int n = 0; n < 8; n += 1) {
-    pinMode(DATA[n], INPUT);
+  for(unsigned int nn = 0; nn < 8; nn++) {
+    pinMode(DATA[nn], INPUT);
   }
   pinMode(CLOCK, INPUT);
   pinMode(READ, INPUT);
@@ -25,11 +29,9 @@ void setup() {
 }
 
 void onClock() {
-  char output[15];
-
   // print the address bus in binary and hex
   uint16_t address = 0;
-  for (int nn = 0; nn < 16; nn++) {
+  for(unsigned int nn = 0; nn < 16; nn++) {
     int bit = digitalRead(ADDR[nn]) ? 1 : 0;
     Serial.print(bit);
     address = (address << 1) + bit;
@@ -41,7 +43,7 @@ void onClock() {
 
   // print data bus in binary and hex
   uint8_t data = 0;
-  for (int nn = 0; nn < 8; nn++) {
+  for(unsigned int nn = 0; nn < 8; nn++) {
     int bit = digitalRead(DATA[nn]) ? 1 : 0;
     Serial.print(bit);
     data = (data << 1) + bit;
@@ -52,8 +54,8 @@ void onClock() {
   Serial.print("\t");
 
   // print control data
-  if (!digitalRead(IORQ)) Serial.print("I");
-  if (!digitalRead(MREQ)) Serial.print("M");
+  if (!digitalRead(IORQ))  Serial.print("I");
+  if (!digitalRead(MREQ))  Serial.print("M");
   if (!digitalRead(READ))  Serial.print("r");
   if (!digitalRead(WRITE)) Serial.print("w");
 
